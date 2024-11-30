78065312bb0a24a5c1ae7d59e15a1d2dccf84855
dwinanto34 <87220725+dwinanto34@users.noreply.github.com>
2024-11-30 15:54:45 +0900

    Basic Data Type

---
+++ basic/DataType/BasicDataType.py
@@ -0,0 +1,36 @@
+
+########################
+# Input number
+print("\n\n# Input number")
+print("Input number: ")
+input_number = input()
+number = int(input_number)
+print(f"Your input type is: {type(number)} with value {number}")
+
+########################
+# Input string
+print("\n\n# Input string")
+print("Input name: ")
+input_name = input()
+print(input_name)
+
+########################
+# String Interpolation
+print("\n\n# String Interpolation")
+name = "Christian"
+say_hi = f"Hi, {name}"
+print(say_hi)
+
+########################
+# Boolean
+print("\n\n# Boolean")
+exist = False
+print(exist)
+
+########################
+# Float
+print("\n\n# Float")
+float = 0.1
+print(float)
+
+
